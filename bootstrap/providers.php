7dd9a6b1fbf7f0c93168af9172fd2382bb7904cf
Tales Sathler Novais <p.talessathler@gmail.com>
2026-06-04 21:51:14 -0300

    fix: only register TelescopeServiceProvider in local env

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,11 +3,15 @@
 use App\Providers\AppServiceProvider;
 use App\Providers\FortifyServiceProvider;
 use App\Providers\HorizonServiceProvider;
-use App\Providers\TelescopeServiceProvider;
 
-return [
+$providers = [
     AppServiceProvider::class,
     FortifyServiceProvider::class,
     HorizonServiceProvider::class,
-    TelescopeServiceProvider::class,
 ];
+
+if (app()->environment('local')) {
+    $providers[] = App\Providers\TelescopeServiceProvider::class;
+}
+
+return $providers;
